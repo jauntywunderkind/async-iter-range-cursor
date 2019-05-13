8beae0ebfbb42bcecafc23455bbbbaab0036a546
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-05-13 17:24:09 -0400

    Make cursor an abstract producer of values.

--- cursor.js
+++ cursor.js
@@ -1,7 +1,6 @@
 "use module"
 import Deferrant from "deferrant"
 
-
 /**
 * Produce an async iterator that
 */
@@ -16,70 +15,89 @@ export function Cursor( arg= Number.POSITIVE_INFINITY){
 	}
 
 	async function *cursor(){
-		const target= cursor.target
-		while( !target.isEnd()){
-			if( target.queue.length){
-				const val= target.queue.shift()
+		while( !cursor.isEnd()){
+			if( cursor.queue.length){
+				// capture what we have to dispatch now
+				const drain= cursor.queue
+				// set enqueue to be 
+				cursor.queue= cursor.makeQueue()
+				// return anything ready to dispatch
+				for( const item of drain){
+					yield item
+				}
+				const val= cursor.queue.shift()
 				yield val
 			}else{
-				target.waiting= Deferrant()
-				await target.waiting
+				cursor.waiting= Deferrant()
+				await cursor.waiting
 			}
 		}
 	}
-	
-	cursor.target= args.target? args.target( cursor)|| cursor
+	// produce additional iterations that may here-after be consumed
+	cursor.step= function( iterations= 1){
+		if (iterations<= 0){
+			return
+		}
+
+		// produce 
+		let isEnd= cursor.isEnd()
+		while( !isEnd&& iterations> 0){
+			--iterations
+			const val= cursor.produce()
+			let isEnd= cursor.isEnd()
+			if( !isEnd){
+				cursor.queue.push( val)
+			}
+		}
+		// let anyone who was waiting for us know there are now iterations
+		if( cursor.waiting){
+			const oldWaiting= cursor.waiting
+			cursor.waiting= null
+			cursor.waiting.resolve()
+		}
+	}
+
+	// defaults
 	cursor.begin= 0
 	cursor.end= Number.POSITIVE_INFINITY
 	cursor.next= undefined // next value to enqueue
-	cursor.queue= [] // values ready to be consumed
+	cursor.queue= null // values ready to be consumed
 	cursor.waiting= null // defer if we need to wait for an enqueue
-	cursor.enqueue= function( iterations= 1){
-		const target= this.target
-		while( !target.isEnd()&& iterations> 0){
-			--iterations
-			target.next= target.increment()
-			if( !target.isEnd()){
-				cursor.queue.push( target.next)
-			}
-		}
-		if( cursor.waiting){
-			cursor.waiting.resolve()
-			cursor.waiting= null
-		}
-	}
-	cursor.reset= function(){
-		const target= this.target
-		target.next= target.begin
-		return this
-	}
-	cursor.end= function(){
-		const target= this.target
-		if( target.next< target.end){
-			targt.next= target.end
-		}
-		return this
-	}
-	cursor.increment= function(){
-		return cursor.next++
-	}
-	cursor.isEnd= function(){
-		return this.target.next>= this.target.end
+	cursor.makeQueue= function(){
+		return []
 	}
 	cursor.clone= function(){
 		const
-		  target= this.target,
-		  queue= target.queue.clone? target.queue.clone(): [ ...target.queue],
-		  args= const args= { ...target, queue}
+		  queue= cursor.queue.clone? cursor.queue.clone(): [ ...cursor.queue],
+		  args= { ...cursor, queue}
 		return new Cursor( args)
 	}
+
+	// standard "range" implementation of our producer
+	cursor.produce= function(){
+		return cursor.next++
+	}
+	cursor.reset= function(){
+		cursor.next= cursor.begin
+	}
+	cursor.end= function(){
+		if( cursor.next< cursor.end){
+			cursor.next= cursor.end
+		}
+	}
+	cursor.isEnd= function(){
+		return cursor.next>= cursor.end
+	}
+
+	// copy in anything from args
 	for( const [prop, val] of args.entries()){
 		cursor[ prop]= val
 	}
-	this.reset()
-	return cursor()
+	cursor.queue= makeQueue()
+	cursor.reset()
+	return cursor
 }
 export {
-  Range as cursor
+  Cursor as cursor
 }
-export default cursor
+export default Cursor
